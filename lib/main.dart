d2e13ae8cb041edcc946f5fba6879fdf6bcf03ec
Ranjeet <ranjeet@gmail.com>
2020-03-28 13:05:43 +0530

    Added raisedButton widgets & list items

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,14 @@ class MyApp extends StatelessWidget
         appBar: AppBar(
           title:Text('My First App'),
           ),
-        body: Text('This is default text!'),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(child: Text('Answer 1'),onPressed: null,),
+            RaisedButton(child: Text('Answer 2'),onPressed: null,),
+            RaisedButton(child: Text('Answer 3'),onPressed: null,),
+          ],
+        ),
         ),
       );
   }
