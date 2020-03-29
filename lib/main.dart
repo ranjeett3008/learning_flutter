d9bf4f4b5debc35c4d712bdf46936f1f17d40b56
Ranjeet <ranjeet@gmail.com>
2020-03-29 10:18:58 +0530

    State full widget added...

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,41 @@ import 'package:flutter/material.dart';
 // }
 
 void main() => runApp(MyApp());
-class MyApp extends StatelessWidget 
+class MyApp extends StatefulWidget 
 {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestion(){
+    setState(() {
+       questionIndex = questionIndex + 1;
+    });
+   
+    print(questionIndex);
+  }
+
+  
+  @override
   Widget build(BuildContext context){
+    var questions = [
+      "What's your favorite color?",
+      "what's your favorite animal?"
+    ];
       return MaterialApp(home:Scaffold(
         appBar: AppBar(
           title:Text('My First App'),
           ),
         body: Column(
-          children: <Widget>[
-            Text('The question!'),
-            RaisedButton(child: Text('Answer 1'),onPressed: null,),
-            RaisedButton(child: Text('Answer 2'),onPressed: null,),
-            RaisedButton(child: Text('Answer 3'),onPressed: null,),
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(child: Text('Answer 1'),onPressed: answerQuestion,),
+            RaisedButton(child: Text('Answer 2'),onPressed: answerQuestion,),
+            RaisedButton(child: Text('Answer 3'),onPressed: answerQuestion,),
           ],
         ),
         ),
